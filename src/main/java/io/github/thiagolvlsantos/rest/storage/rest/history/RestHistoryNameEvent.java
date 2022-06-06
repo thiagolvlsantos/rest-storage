37a4c0e4bcf5fb9e53dacdbad0157c78cb8f9533
thiagolvlsantos <thiago.lvl.santos@gmail.com>
2022-06-06 01:07:48 -0300

    Bug fix, events should be totally independent.

--- src/main/java/io/github/thiagolvlsantos/rest/storage/rest/history/RestHistoryNameEvent.java
+++ src/main/java/io/github/thiagolvlsantos/rest/storage/rest/history/RestHistoryNameEvent.java
@@ -1,14 +1,16 @@
 package io.github.thiagolvlsantos.rest.storage.rest.history;
 
+import io.github.thiagolvlsantos.rest.storage.rest.AbstractRestEvent;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @SuppressWarnings("serial")
-public class RestHistoryNameEvent<T> extends RestHistoryEvent<T> {
+public class RestHistoryNameEvent<T> extends AbstractRestEvent<T> {
 
 	private String name;
+	private String paging;
 
 	public RestHistoryNameEvent(Object source) {
 		super(source);
--- src/main/java/io/github/thiagolvlsantos/rest/storage/rest/history/RestHistoryResourceEvent.java
+++ src/main/java/io/github/thiagolvlsantos/rest/storage/rest/history/RestHistoryResourceEvent.java
@@ -1,13 +1,16 @@
 package io.github.thiagolvlsantos.rest.storage.rest.history;
 
+import io.github.thiagolvlsantos.rest.storage.rest.AbstractRestEvent;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @SuppressWarnings("serial")
-public class RestHistoryResourceEvent<T> extends RestHistoryNameEvent<T> {
+public class RestHistoryResourceEvent<T> extends AbstractRestEvent<T> {
 
+	private String name;
+	private String paging;
 	private String path;
 
 	public RestHistoryResourceEvent(Object source) {
